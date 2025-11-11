b8358853e762672d97c969de31408aa3988a63ff
ktapia576 <ktapia576@hotmail.com>
2025-11-11 09:17:10 -0500

    test html escaping

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from markupsafe import escape
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, World!"
+    name = request.args.get("name", "Flask")
+    return f"Hello, {escape(name)}!"
