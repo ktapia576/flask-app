4dc193e134414c6c9972856e0bad88bb0b86c1e4
ktapia576 <ktapia576@hotmail.com>
2025-11-12 08:11:56 -0500

    Add simple login page

--- app.py
+++ app.py
@@ -16,3 +16,17 @@ def about():
 @app.route('/hello/<name>')
 def hello(name=None):
     return render_template('hello.html', person=name)
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if valid_login(request.form['username'],
+                       request.form['password']):
+            return log_the_user_in(request.form['username'])
+        else:
+            error = 'Invalid username/password'
+    # the code below is executed if the request method
+    # was GET or the credentials were invalid
+    return render_template('login.html', error=error)
